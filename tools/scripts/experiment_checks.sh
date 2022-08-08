d85b9ff6c89eca04f804a2c04e141d2d6782eb84
Addison Snelling <asnell@hpe.com>
2022-08-08 14:21:17 -0500

    install validation: core api, deepspeed, pytorch experiment checks

---
+++ tools/scripts/experiment_checks.sh
@@ -0,0 +1,87 @@
+#!/bin/sh
+
+set -e
+
+readonly tmpdir="$(mktemp -d)"
+echo "created temporary directory ${tmpdir}" >&2
+
+on_exit() {
+    echo "removing temporary directory: ${tmpdir}" >&2
+    rm -rf "${tmpdir}"
+    echo "exiting health checks" >&2
+}
+
+trap on_exit EXIT
+
+if [ -z "${DETVALID_SLOTS_PER_NODE}" ]; then
+    echo "ERROR: environment variable DETVALID_SLOTS_PER_NODE not set." >&2
+fi
+
+core_api="./examples/tutorials/core_api"
+core_api_single_config="${core_api}/0_start.yaml"
+core_api_distributed_config="${core_api}/4_distributed.yaml"
+
+mnist_pytorch="./examples/tutorials/mnist_pytorch"
+mnist_pytorch_single_config="${mnist_pytorch}/const.yaml"
+mnist_pytorch_distributed_config="${mnist_pytorch}/distributed.yaml"
+
+deepspeed="./examples/deepspeed/cifar10_moe"
+deepspeed_single_config="${deepspeed}/moe.yaml"
+deepspeed_distributed_config="${deepspeed}/moe.yaml"
+
+mnist_pytorch="./examples//mnist_pytorch"
+mnist_pytorch_single_config="${mnist_pytorch}/const.yaml"
+mnist_pytorch_distributed_config="${mnist_pytorch}/distributed.yaml"
+
+mgpu_mnode_num_slots=$(( ${DETVALID_SLOTS_PER_NODE} * 2 ))
+
+
+# ====================
+# Experiment: Core API
+# ====================
+
+# COM.EXP.CORE_API.SGPU_SNODE
+det e create --test-mode "${core_api_single_config}" "${core_api}"
+
+# COM.EXP.CORE_API.MGPU_SNODE
+det e create --test-mode --config "resources.slots_per_trial=2" "${core_api_distributed_config}" "${core_api}"
+
+# COM.EXP.CORE_API.MGPU_MNODE
+det e create --test-mode --config "resources.slots_per_trial=${mgpu_mnode_num_slots}" "${core_api_distributed_config}" "${core_api}"
+
+# ===================
+# Experiment: PyTorch
+# ===================
+
+# COM.EXP.PYTORCH.SGPU_SNODE
+det e create --test-mode "${pytorch_single_config}" "${pytorch}"
+
+# COM.EXP.PYTORCH.MGPU_SNODE
+det e create --test-mode --config "resources.slots_per_trial=2" "${pytorch_distributed_config}" "${pytorch}"
+
+# COM.EXP.PYTORCH.MGPU_MNODE
+det e create --test-mode --config "resources.slots_per_trial=${mgpu_mnode_num_slots}" "${pytorch_distributed_config}" "${pytorch}"
+
+# =====================
+# Experiment: DeepSpeed
+# =====================
+
+# COM.EXP.DEEPSPEED.SGPU_SNODE
+det e create --test-mode "${deepspeed_single_config}" "${deepspeed}"
+
+# COM.EXP.DEEPSPEED.MGPU_SNODE
+det e create --test-mode --config "resources.slots_per_trial=2" "${deepspeed_distributed_config}" "${deepspeed}"
+
+# COM.EXP.DEEPSPEED.MGPU_MNODE
+det e create --test-mode --config "resources.slots_per_trial=${mgpu_mnode_num_slots}" "${deepspeed_distributed_config}" "${deepspeed}"
+
+# =====
+# Tasks
+# =====
+
+# COM.TSK.START_SHELL
+shell_id="$(echo exit
+        | det shell start 2>&1
+        | grep --max-count=1 -Eo '[0-9a-f]{8}(-[0-9a-f]{4}){3}-[0-9a-f]{12}')"
+    det shell logs "${shell_id}" | grep "disconnected by user"
+    det shell kill "${shell_id}"
